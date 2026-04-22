69d8f4ef73248f5894e6fd86121966f2fbd78858
Huy (Been) <117251272+NguyenQuangHuy08@users.noreply.github.com>
2026-04-22 22:04:05 +0700

    feat: git commit

---
+++ config.js
@@ -0,0 +1,21 @@
+const CONFIG = {
+  introTitle: "Công chúa của anh!",
+
+  introDesc: `Công chúa yêu dấu của anh à, Em có biết được là anh tìm kiếm em khó thế nào không ?
+  Anh mong được rằng là sẽ được ở bên cạnh em, yêu thương chia sẻ động viên em !`,
+  btnIntro: "YÊU EM",
+
+  title: "Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙",
+  desc: "Từ khi gặp em, anh biết được những câu chuyện mà em chia xẻ, anh luôn muốn hiểu em hơn, anh mong rằng hiện tại và sau này sẽ có em ở bên cạnh anh!!!!!!!! Em đồng ý làm người yêu của anh nhé",
+ 
+ 
+  btnYes: "Em đồng ý HIHI <33",
+  btnNo: "Không, Anh trai à :3",
+  question: "Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3",
+  btnReply: "YÊU EM <3",
+
+  reply: "Yêu thì yêu mà không yêu thì yêu <33333333",
+  mess: "Anh biết mà 🥰. Anh cũng yêu em nhiều nhiều lắm ạ😘😘",
+  messDesc: "Cuối tuần anh chở công chúa của anh đi chơi với nhá !",
+  btnAccept: "Okiiiii lun <3",
+};
